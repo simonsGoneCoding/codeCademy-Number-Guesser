9d15f16977a12e484270c1ea34ba4e34f17fa889
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-03-25 16:01:41 +0000

    Added logics according to spec.

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(human, computer, secret) {
+  const humanDifference = Math.abs(secret - human);
+  const compDifference = Math.abs(secret - computer);
+  return humanDifference <= compDifference ? true : false;
+}
+
+function updateScore(win) {
+  switch (win) {
+    case "human":
+      return humanScore++;
+      break;
+
+    case "computer":
+      return computerScore++;
+      break;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
